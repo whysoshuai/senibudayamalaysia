c3f5dda77d005f589083612abea0a4b64486525c
whysoshuai <144249493+whysoshuai@users.noreply.github.com>
2023-11-09 20:12:00 +0800

    update

--- script.js
+++ script.js
@@ -14,8 +14,32 @@ window.onload = function () {
 				emailjs.sendForm('service_r24gb87', 'template_itl7fqb', this)
 						.then(function () {
 								console.log('SUCCESS!');
+							document.getElementById("success-message").classList.remove("hidden");
 						}, function (error) {
 								console.log('FAILED...', error);
 						});
 		});
+}
+
+//back to top
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+function myFunction() {
+	alert("Thanks for your feedback!");
 }
