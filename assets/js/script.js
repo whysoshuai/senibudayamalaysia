b67ad22d3c18152d174e24b529ae88354810973e
whysoshuai <issactanshuai@outlook.com>
2024-05-11 23:53:24 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,51 @@
 (function () {
-		// https://dashboard.emailjs.com/admin/account
-		emailjs.init('5ty_urpEcIa3TnyjO');
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("5ty_urpEcIa3TnyjO");
 })();
 
 window.onload = function () {
-		document.getElementById('email-form').addEventListener('submit', function (event) {
-				event.preventDefault();
-				// generate a five digit number for the contact_number variable
-				this.contact_number.value = Math.random() * 100000 | 0;
-				// these IDs from the previous steps
-				emailjs.sendForm('service_r24gb87', 'template_itl7fqb', this)
-						.then(function () {
-								console.log('SUCCESS!');
-							document.getElementById("success-message").classList.remove("hidden");
-						}, function (error) {
-								console.log('FAILED...', error);
-						});
-		});
-}
+  document
+    .getElementById("email-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_r24gb87", "template_itl7fqb", this).then(
+        function () {
+          console.log("SUCCESS!");
+          document.getElementById("success-message").classList.remove("hidden");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
 
 //back to top
 
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		mybutton.style.display = "block";
-	} else {
-		mybutton.style.display = "none";
-	}
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
 function topFunction() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function myFunction() {
-	alert("Thanks for your feedback!");
+  alert("Thanks for your feedback!");
 }
 
 var audio = document.getElementById("bgmusic");
@@ -47,15 +53,31 @@ audio.volume = 0.1;
 
 //cool animation
 AOS.init({
-	duration: 3000,
-	once: true,
+  duration: 3000,
+  once: true,
 });
 
 // translate func
-function googleTranslateElementInit(){
-	new google.translate.TranslateElement({
-		pageLanguage: 'en'
-	},
-	'google_translate_element'
-	);
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "en",
+    },
+    "google_translate_element"
+  );
 }
+
+window.onload = function () {
+  var pageTitle = document.title;
+  var attentionMessage = "Come Back!";
+
+  document.addEventListener("visibilitychange", function (e) {
+    var isPageActive = !document.hidden;
+
+    if (!isPageActive) {
+      document.title = attentionMessage;
+    } else {
+      document.title = pageTitle;
+    }
+  });
+};
